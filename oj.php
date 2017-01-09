892a0a82b1c2f66c08b9f32038bd0b841297cb99
Himanshu Mishra <mishrah225@gmail.com>
2017-01-09 22:23:33 +0530

    printing outputs

--- oj.php
+++ oj.php
@@ -1,20 +1,32 @@
 <?php
-
 $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => 'api.hackerrank.com/checker/submission.json',
-    //CURLOPT_USERAGENT => 'Codular Sample cURL Request',
     CURLOPT_POST => 1,
     CURLOPT_POSTFIELDS => array(
         'source' => $_GET["name"],
         'lang'=>'2',
-        'testcases'=>'["1","2","3","345678"]',
-        'api_key'=>'hackerrankKey'
+        'testcases'=>'["1","3","5"]',
+        'api_key'=>'hackerrank|597045-1168|fe47359f9224d36df1ff58a1574e6c1fa5c15530'
     )
 ));
 $resp = curl_exec($curl);
-// Close request to clear up some resources
 curl_close($curl);
-echo "$resp";
+//echo "$resp";
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($resp, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($jsonIterator as $key => $val) {
+    if($key=="stdout")
+    {
+    	if(is_array($val))
+    	{
+    	foreach ($val as &$value) {
+    		echo "<pre> $value </pre>";
+		}
+		}
+    }
+}
 ?>
